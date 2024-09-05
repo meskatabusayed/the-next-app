17691369aa36dd647c39bc7dd3580a5c45e279e1
meskatabusayed <meskatabusayed@gmail.com>
2024-09-05 17:31:22 +0530

    data fetching done

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,36 @@
-import Counter from "@/components/count/Counter";
+import React from "react";
 
+const page = async () => {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
 
-
-import React from 'react'
-
-const page = () => {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-          <Counter/>
+      <div className="flex gap-8 px-4">
+        {shoes?.slice(0, 3).map((shoe) => (
+          <div key={shoe.id} className="card bg-base-100 w-80 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
 export default page;
-
